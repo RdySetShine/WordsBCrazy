0f833f5f8e73d95cfd386b886ac5bcfff43aec6d
RdySetShine <126523453+RdySetShine@users.noreply.github.com>
2023-07-24 21:48:27 -0400

    uploading finished product

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,39 +1,23 @@
 const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+
 window.addEventListener("beforeinstallprompt", (event) => {
-  // Stash the event so it can be triggered later
-  window.deferredPrompt = event;
-  // Update UI notify the user they can add to home screen
-  butInstall.disabled = false;
+  window.defferedPrompt = event;
+  butInstall.style.visibility = "visible";
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener("click", async () => {
-  // Hide the app provided install promotion
-  butInstall.disabled = true;
+  const promptEvent = window.defferedPrompt;
 
-  // Show the install prompt
-  const { userChoice } = await window.deferredPrompt.prompt();
-  console.log(userChoice);
-
-  // Wait for the user to respond to the prompt
-  if (userChoice.outcome === "accepted") {
-    console.log("User accepted the A2HS prompt");
+  if (!promptEvent) {
+    return;
   }
-
-  // Clear the deferred prompt variable
-  window.deferredPrompt = null;
+  promptEvent.prompt();
+  window.defferedPrompt = null;
+  butInstall.classList.toggle("hidden", true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {
-  console.log("PWA was installed");
-
-  // Clear the deferredPrompt
-  window.deferredPrompt = null;
-
-  // Hide the install button
-  butInstall.disabled = true;
+  window.defferedPrompt = null;
 });
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,39 +2,14 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// Add a plugin for a service worker and manifest file.
-const serviceWorkerPlugin = new InjectManifest({
-  swSrc: "./src-sw.js",
-  swDest: "sw.js",
-});
-
-const manifestPlugin = new WebpackPwaManifest({
-  name: "Just Another Text Editor",
-  short_name: "J.A.T.E.",
-  description: "Takes notes with JavaScript syntax highlighting.",
-  publicPath: "/",
-  fingerprints: false,
-  inject: true,
-  background_color: "#225ca3",
-  theme_color: "#225ca3",
-  start_url: "/",
-  icons: [
-    {
-      src: path.resolve("./src/images/logo.png"),
-      sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
-      destination: path.join("assets", "icons"),
-    },
-  ],
-});
-
-// TODO: Add CSS loaders and babel to webpack.
+// DONE: Add and configure workbox plugins for a service worker and manifest file
+// DONE: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: "production",
+    mode: "development",
     entry: {
       main: "./src/js/index.js",
       install: "./src/js/install.js",
@@ -46,33 +21,48 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "PWA Text Editor",
+        title: "Webpack Plugin",
       }),
-      new MiniCssExtractPlugin(),
-      serviceWorkerPlugin,
-      manifestPlugin,
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "service-worker.js",
+      }),
+      new WebpackPwaManifest({
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "A text editor for online and offline use.",
+        background_color: "#7eb4e2",
+        theme_color: "#7eb4e2",
+        start_url: "/",
+        publicPath: "/",
+        fingerprints: false,
+        inject: true,
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, "css-loader"],
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: "asset/resource",
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /node_modules/,
           use: {
             loader: "babel-loader",
             options: {
               presets: ["@babel/preset-env"],
-              plugins: [
-                "@babel/transform-runtime",
+              plugins:[
                 "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
               ],
             },
           },
