699496a70526da3460698cf4639148254c73df84
RdySetShine <126523453+RdySetShine@users.noreply.github.com>
2023-08-04 11:17:29 -0400

    updated src-sw.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,4 +1,4 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+const { warmStrategyCache } = require('workbox-recipes');
 const { CacheFirst } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
@@ -27,4 +27,19 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute(({ request }) => request.destination === "image", pageCache);
+// registerRoute(({ request }) => request.destination === "image", pageCache);
+
+
+new CacheFirst({
+  cacheName: 'my-image-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxEntries: 60,
+      // for 30days
+      maxAgeSeconds: 30 * 24 * 60 * 60, 
+    }),
+  ],
+})
